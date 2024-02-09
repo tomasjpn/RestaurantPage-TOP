b227107521db588b8c01bebe85998b4877856e00
tomasjpn <tomasphammc@gmail.com>
2024-02-09 21:03:18 +0100

    Added CSS

--- src/Menu.js
+++ src/Menu.js
@@ -14,6 +14,7 @@ export function menuPage() {
 
     // p Element 
     const pElm = document.createElement("p");
+    pElm.id ="pElmMenu"
     pElm.textContent = "McDonald's is one of the world's largest fast food chains, founded in 1940 in San Bernardino, California, and incorporated in Des Plaines, Illinois, in 1955.[1] Since then, McDonald's has become a household name in America, known for selling a variety of convenience food items at thousands of locations worldwide. Throughout its history, McDonald's has experimented with a number of different offerings on the menu.[2] In 2007, McDonald's had 85 items on its menu;[where?] by 2013 this number had risen to 145 items on its menu.[3]";
     menuDiv.appendChild(pElm);
 }
