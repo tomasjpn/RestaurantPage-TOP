1e30e00b1f0be03c3ec1f85bef594495277f9d10
tomasjpn <tomasphammc@gmail.com>
2024-02-10 02:03:47 +0100

    Merge remote-tracking branch 'origin/master'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import { RestaurantHomePage } from "./page.js";
 import { createTabs } from "./tabs";
-<<<<<<< HEAD
-import "./styles.css";
-=======
->>>>>>> e6e7f8189b3edbda4b8045214f58fd593ef4fa92
+import "./styless.css";
 
 RestaurantHomePage();
 createTabs();
